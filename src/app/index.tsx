bee707f79e285a9e60fe8b0d1fe156837510d0fd
LEVI JOSE DE MELO AGUIAR <levi.jmaguiar@senacsp.edu.br>
2026-03-24 21:50:15 -0300

    ok

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Home(){
+    return (
+        <View className="bg-slate-900 flex-1">
+            <Text className="text-white"> Hello react Native </Text>
+        </View>
+    )
+}
